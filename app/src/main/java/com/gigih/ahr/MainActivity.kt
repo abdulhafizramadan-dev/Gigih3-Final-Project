b5b71dd84b19f200aa7b67eaaa9374d60d009811
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2023-07-15 16:07:58 +0700

    feat(home-bottom-sheet): Adding home bottom sheet

--- app/src/main/java/com/gigih/ahr/MainActivity.kt
+++ app/src/main/java/com/gigih/ahr/MainActivity.kt
@@ -3,7 +3,13 @@ package com.gigih.ahr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.gigih.ahr.databinding.ActivityMainBinding
+import com.gigih.ahr.util.gone
+import com.gigih.ahr.util.setMargins
+import com.gigih.ahr.util.visible
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,15 +18,60 @@ class MainActivity : AppCompatActivity() {
    }
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomBehavior: BottomSheetBehavior<ConstraintLayout>
+
+    private val bottomSheet get() =  binding.bsLatestDisaster
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        bottomBehavior = BottomSheetBehavior.from(binding.bsLatestDisaster.root)
+
+        initUi()
+        initAction()
+        bottomSheetConfiguration()
+
         binding.homeAppbar.setOnDisasterQueryChanged {
             Log.d(TAG, "onCreate: Query = $it")
         }
 
     }
+
+    private fun initUi() {
+        bottomBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+    }
+
+    private fun initAction() {
+        binding.bsLatestDisaster.btnHideBs.setOnClickListener {
+            bottomBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
+
+    private fun bottomSheetConfiguration() {
+        bottomBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> toggleBottomSheetLatestDisaster(true)
+                    else -> toggleBottomSheetLatestDisaster(false)
+                }
+            }
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+        })
+    }
+
+    private fun toggleBottomSheetLatestDisaster(state: Boolean) {
+        if (state) {
+            val dimen32dp = resources.getDimension(R.dimen.dimen_32dp)
+            bottomSheet.tvLabelLatestDisaster.setMargins(start = dimen32dp.toInt())
+            bottomSheet.btnHideBs.visible()
+            bottomSheet.btnHideBs.animate().alpha(1f)
+        } else {
+            bottomSheet.tvLabelLatestDisaster.setMargins(start = 0)
+            bottomSheet.btnHideBs.gone()
+            bottomSheet.btnHideBs.animate().alpha(0f)
+        }
+    }
+
 }
--- app/src/main/java/com/gigih/ahr/util/ViewExt.kt
+++ app/src/main/java/com/gigih/ahr/util/ViewExt.kt
@@ -2,6 +2,31 @@ package com.gigih.ahr.util
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.marginBottom
+import androidx.core.view.marginEnd
+import androidx.core.view.marginStart
+import androidx.core.view.marginTop
 
-val Context.layoutInflater get() =
-    LayoutInflater.from(this)
+val Context.layoutInflater: LayoutInflater
+    get() = LayoutInflater.from(this)
+
+fun View.setMargins(
+    start: Int = marginStart,
+    top: Int = marginTop,
+    end: Int = marginEnd,
+    bottom: Int = marginBottom,
+) {
+    layoutParams = (layoutParams as ViewGroup.MarginLayoutParams).apply {
+        setMargins(start, top, end, bottom)
+    }
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
