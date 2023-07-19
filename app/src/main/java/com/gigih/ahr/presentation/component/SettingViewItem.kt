b1357c0e1e81f295a923acbf7cbb0103fa9f343b
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2023-07-19 23:18:57 +0700

    refactor(settings-screen): Adding setting item view set property method

--- app/src/main/java/com/gigih/ahr/presentation/component/SettingViewItem.kt
+++ app/src/main/java/com/gigih/ahr/presentation/component/SettingViewItem.kt
@@ -16,6 +16,7 @@ class SettingViewItem(context: Context, attrs: AttributeSet?) : ConstraintLayout
 
     private var title: String = ""
     private var subtitle: String = ""
+    private var checked: Boolean = false
 
     private val attributes: TypedArray by lazy {
         context.obtainStyledAttributes(attrs, R.styleable.SettingViewItem, 0, 0)
@@ -29,13 +30,30 @@ class SettingViewItem(context: Context, attrs: AttributeSet?) : ConstraintLayout
     private fun initAttributes() {
         title = attributes.getString(R.styleable.SettingViewItem_title) ?: ""
         subtitle = attributes.getString(R.styleable.SettingViewItem_subtitle) ?: ""
+        checked = attributes.getBoolean(R.styleable.SettingViewItem_checked, false)
     }
 
     private fun initUi() {
         with(binding) {
             tvTitle.text = title
             tvSubtitle.text = subtitle
+            swSetting.isChecked = checked
         }
+    }
+
+    fun setTitle(title: String) {
+        this.title = title
+        initUi()
+    }
+
+    fun setSubtitle(subtitle: String) {
+        this.subtitle = subtitle
+        initUi()
+    }
+
+    fun setChecked(checked: Boolean) {
+        this.checked = checked
+        initUi()
     }
 
 }
--- app/src/main/java/com/gigih/ahr/presentation/setting/SettingsActivity.kt
+++ app/src/main/java/com/gigih/ahr/presentation/setting/SettingsActivity.kt
@@ -1,12 +1,16 @@
 package com.gigih.ahr.presentation.setting
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.gigih.ahr.R
+import androidx.appcompat.app.AppCompatActivity
+import com.gigih.ahr.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
